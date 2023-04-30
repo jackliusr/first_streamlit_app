fdbfb73e05928088c07e91119c0f1d0b86fffe90
Jack Liu Shurui <jackliusr@gmail.com>
2023-04-30 21:51:13 +0800

    special menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,11 @@
 import streamlit
 
 streamlit.title("My Marents New Health Diner")
+
 streamlit.header('Breakfast Menu')
 streamlit.text('🥣Omega 3 & Buleberry Oatmeal')
 streamlit.text('🥗kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞Avocado Toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
