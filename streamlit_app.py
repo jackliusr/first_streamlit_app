375134bf15321b9824b0931da56290ba62796188
Jack Liu Shurui <jackliusr@gmail.com>
2023-04-30 21:43:20 +0800

    breakfast menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title("My Marents New Health Diner")
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Buleberry Oatmeal')
+streamlit.text('kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
