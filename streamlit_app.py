019b629821cb7dac405de34535efa2c7161b15c8
Jack Liu Shurui <jackliusr@gmail.com>
2023-04-30 21:48:51 +0800

    emojis

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,8 @@
 import streamlit
 
 streamlit.title("My Marents New Health Diner")
-
 streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Buleberry Oatmeal')
-streamlit.text('kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.text('🥣Omega 3 & Buleberry Oatmeal')
+streamlit.text('🥗kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞Avocado Toast')
